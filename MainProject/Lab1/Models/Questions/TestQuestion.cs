7c978b9f3af65b337f16a14f4165c2d7d24d7510
Troy Riblett <troy.riblett@oit.edu>
2016-11-07 18:24:52 -0800

    Updated Exam
    Added some divs and set hidden field for ID

--- MainProject/Lab1/Models/Questions/TestQuestion.cs
+++ MainProject/Lab1/Models/Questions/TestQuestion.cs
@@ -4,11 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace CST465
 {
     public class TestQuestion
     {
+        [HiddenInput(DisplayValue = false)]
         public int ID { get; set; }
         public string Question { get; set; }
         public virtual string Answer { get; set; }
